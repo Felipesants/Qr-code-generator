5459e530c916119d5cf5f02d3f70590eeae2dd42
Flpps2 <86977960+Felipesants@users.noreply.github.com>
2022-05-28 14:40:22 -0300

    commit concluído

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@ function GerarQRcode(){
 
     var inputUser = document.querySelector('textarea').value;
 
+    document.getElementById('txtQrcode').innerHTML = document.querySelector('textarea').value;
+
     //Api gerador de QR-code Google
-    var APIqrcode = 'https://chart.googleapis.com/chart?cht=qr&chs=500x500&chl=';
+    var APIqrcode = 'https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=';
 
     var conteudoQRcode = APIqrcode + encodeURIComponent(inputUser);
 
     document.querySelector('#QRCodeImage').src = conteudoQRcode;
 
 }
+
